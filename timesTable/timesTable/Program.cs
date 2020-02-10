89a7179cecd5dd0a5eabce16cb3cf4cb94357eba
1321200 <55232721+A2JA2@users.noreply.github.com>
2020-02-10 10:27:34 +0000

    Add files via upload
    this is a Console App (NET Framework)

---
+++ timesTable/timesTable/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace timesTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberFixed, numberInput;
+
+            Console.Write("\n\n");
+            Console.Write("Display the multiplication table:\n");
+            Console.Write("------------------------------");
+            Console.Write("\n\n");
+
+            Console.Write("Input the number (Table to be calculated) : ");
+
+            //input the number and convert to integer
+            numberInput = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n");
+
+            for (numberFixed = 1; numberFixed <= 10; numberFixed++)//this is the loop, the loop ends when the fixed number is greater than 10.
+            {
+                Console.Write("{0} x {1} = {2} \n", numberInput, numberFixed, numberInput * numberFixed);
+            }
+            Console.ReadLine();
+        }
+    }
+}
